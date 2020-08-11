9afdb843c1f67574603739776f38e7f0394e7646
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-08-11 13:12:32 -0400

    Update DayTripGenerator.js
    Updated all random functions.  Program will now randomly select random: Resturant, Entertainment, Destination and Transportation.

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -5,18 +5,46 @@
 runprogram ();
 
 
+
 function runprogram () {       
     let randomDestination = ['Queens, NY', 'Tampa, FL', 'Virginia Beach, VA', 'Napa Valley, CA'];
-    let randomNumber = randomDestination[Math.floor(Math.random() * randomDestination.length - 1)];
-        if()       console.log(randomNumber);{
+    let randomNumber = randomDestination[Math.floor(Math.random() * randomDestination.length)];{
 
+        console.log(randomNumber);
 
-
-        }
-        else {
-
-
-        }
-
+        let randomRestaurant = selectRandomRestaurant();
     }
-function 
+}
+
+    
+function selectRandomRestaurant () {
+    let randomRestaurant = ['Firehouse Subs', 'Applebees', 'Olive Garden', 'Bonefish Grill'];
+    let randomNumbers = randomRestaurant[Math.floor(Math.random() * randomRestaurant.length)];{
+    
+        console.log(randomNumbers);
+
+        let randomTranspo = selectRandomTranspo ();
+    }
+}
+    
+
+function selectRandomTranspo () {
+    let randomTranspo = ['Subway', 'Airplane', 'Boat', 'Dragon'];
+    let randomNumbersS = randomTranspo[Math.floor(Math.random() * randomTranspo.length)];{
+            
+         console.log(randomNumbersS);
+         
+         let randomEntertainment = selectRandomEntertainment ();
+        }
+    }
+
+
+function selectRandomEntertainment () {
+    let randomEntertainment = ['Movies', 'Mini Golf', 'Baseball Game', 'Drinks'];
+    let randomNumbersSS = randomEntertainment[Math.floor(Math.random() * randomEntertainment.length)];{
+                
+        console.log(randomNumbersSS);
+             
+            }
+        }
+    
