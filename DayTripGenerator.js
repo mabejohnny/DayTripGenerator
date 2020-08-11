e1e83db1aa808f4fa44ec43fd60faeb42d3a3f04
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-08-11 12:23:46 -0400

    randomDestinationFunction
    This function will provide the user with a randomly generated destination from the array 'randomDestinstion'

---
+++ DayTripGenerator.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+
+runprogram ();
+
+
+function runprogram () {       
+    let randomDestination = ['Queens, NY', 'Tampa, FL', 'Virginia Beach, VA', 'Napa Valley, CA'];
+    let randomNumber = randomDestination[Math.floor(Math.random() * randomDestination.length - 1)];
+        if()       console.log(randomNumber);{
+
+
+
+        }
+        else {
+
+
+        }
+
+    }
+function 
