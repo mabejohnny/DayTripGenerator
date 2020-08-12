eb696508cd28c51695c433cf7ea5ea72c03aa9cc
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-08-12 13:50:07 -0400

    Update DayTripGenerator.js
    as far as looks, im happy with it.  The only problem that is currently happening is the code not running due to it not reading the variable runProgram().
    I have reached out and awaiting.

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -12,28 +12,37 @@ function destinationLogic () {
     let randomTranspo = ['Subway', 'Airplane', 'Boat', 'Dragon'];
     let transpoChoice = randomTranspo[Math.floor(Math.random() * randomTranspo.length)];
 
-
     let randomEntertain = ['Movies', 'Mini Golf', 'Baseball Game', 'Drinks'];
     let entertainChoice = randomEntertain[Math.floor(Math.random() * randomEntertain.length)];
 
 }
 
 runprogram ();
-//pull destination logic out of runporgram and into its own function
-//Add a return to each individual function
-//Recall the appropriate function in each case in the switchcase
 
-function runprogram () {  
+
+
+
+function runProgram(){
+
+    selectrandomDestination();
+    selectRandomRestaurant();
+    selectRandomTranspo();
+    selectRandomEntertain();
+    reSelectRandomChoices();
+    getUserInputForDestinationChoices ();
+}
+
+function selectrandomDestination() {  
 
     let randomDestination = ['Queens, NY', 'Tampa, FL', 'Virginia Beach, VA', 'Napa Valley, CA'];
     let destinationChoice = randomDestination[Math.floor(Math.random() * randomDestination.length)];{
     
         console.log("your going to visit the exotic " + destinationChoice);
-
+        let randomRestaurant = selectRandomRestaurant();
+       
         return destinationChoice; 
 
-    let randomRestaurant = selectRandomRestaurant();
-    let randomTranspo = selectRandomTranspo ();
+
     }
 }
 
@@ -44,39 +53,39 @@ function selectRandomRestaurant () {
     let restaurantChoice = randomRestaurant[Math.floor(Math.random() * randomRestaurant.length)];{
     
         console.log("You will dine at the world famous " + restaurantChoice);
+        let randomTransportation = selectRandomTransportation ();
 
         return restaurantChoice;
-    
+
 
     }
 }
     
+function selectRandomTransportation () {
 
-function selectRandomTranspo () {
-
-    let randomTranspo = ['Subway', 'Airplane', 'Boat', 'Dragon'];
-    let transpoChoice = randomTranspo[Math.floor(Math.random() * randomTranspo.length)];{
+    let randomTransportation = ['Subway', 'Airplane', 'Boat', 'Dragon'];
+    let transportationChoice = randomTransportation[Math.floor(Math.random() * randomTransportation.length)];{
             
-         console.log("First class all the way while you travel around on a " + transpoChoice);
-        
-         return transpoChoice;
+         console.log("First class all the way while you travel around on a " + transportationChoice);
+         let randomEntertainment = selectRandomEntertainment ();
+         return transportationChoice;
          
-    let randomEntertainment = selectRandomEntertain ();
-    let userInput = reSelectRandomChoices ();
+
     }
 }
 
 
-function selectRandomEntertain () {
+function selectRandomEntertainment () {
 
-    let randomEntertain = ['Movies', 'Mini Golf', 'Baseball Game', 'Drinks'];
-    let entertainChoice = randomEntertain[Math.floor(Math.random() * randomEntertain.length)];{
+    let randomEntertainment = ['Movies', 'Mini Golf', 'Baseball Game', 'Drinks'];
+    let entertainChoice = randomEntertainment[Math.floor(Math.random() * randomEntertainment.length)];{
                 
-        console.log("Your entertainment for the evening will be " + entertainChoice);
-          
-        return entertainChoice
+        console.log("Your entertainment for the evening will be " + entertainmentChoice);
+        let userInput = reSelectRandomChoices ();
+       
+        return entertainmentChoice
   
-
+        
     }
 }
 
@@ -97,7 +106,7 @@ function reSelectRandomChoices () {
 }
 
 
-function switchCase () {
+function getUserInputForDestinationChoices () {
 
     let userInput;
     userInput = prompt("Enter 1 to change destination. 2 to change your restaurant. 3 to change your transportation. 4 to change your entertainment. 5 if you have no idea how you got here, but know you wanna leave")
@@ -105,25 +114,25 @@ function switchCase () {
     switch(userInput)
     {
         case "1":
-            return destinationChoice;
+            
+            let destinationChoice = selectrandomDestination();
             alert("your new destination choice is " + destinationChoice);
             console.log("user changed there destination to: " + destinationChoice);
             break;
         case "2":
-            return restaurantChoice;
+           let newRestaurant = selectRandomRestaurant();
             alert("Your new restaurant choice is " + restaurantChoice);
             console.log("user changed their restaurant to: " + restaurantChoice);
             break;
         case "3":
-            return transpoChoice;
-            let newTrans = selectRandomTranspo();
-            alert("Your new transportation will be: " + transpoChoice);
-            console.log("user changed their transportation to: " + transpoChoice);
+            let newTransportation = selectRandomTransportation();
+            alert("Your new transportation will be: " + transportationChoice);
+            console.log("user changed their transportation to: " + transportationChoice);
             break;
         case "4":
-            return entertainChoice;
-            alert("Your new entertainment will me " + entertainChoice);
-            console.log("user changed their entertainment to " + entertainChoice);
+            let newEntertainment = selectRandomEntertainment();
+            alert("Your new entertainment will me " + entertainmentChoice);
+            console.log("user changed their entertainment to " + entertainmentChoice);
             break;
         case "5":
             alert("That's okay! Your still alright in our book!");
